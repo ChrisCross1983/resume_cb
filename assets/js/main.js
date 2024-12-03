31ce8fbf02827c01e26b6309f6963e14a218f6e4
Chris <christian.borza@students.codeinstitute.net>
2024-12-03 15:23:33 +0100

    Fix bugs regarding visitor counter

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,10 +8,25 @@ fetch("/.netlify/functions/visitorCount")
   });
 
 // Event Listener for CV-Download
-document.getElementById("cv-download").addEventListener("click", function () {
-  fetch("/.netlify/functions/trackDownload", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ timestamp: new Date().toISOString() }),
+document.addEventListener("DOMContentLoaded", function () {
+  const downloadLinks = document.querySelectorAll(
+    '[data-bs-target="#cvDownloadModal"]'
+  );
+
+  downloadLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      fetch("/.netlify/functions/trackDownload", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ timestamp: new Date().toISOString() }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Download tracked:", data);
+        })
+        .catch((error) => {
+          console.error("Error tracking download:", error);
+        });
+    });
   });
 });
